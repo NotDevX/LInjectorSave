92aa94e3404069811b0072b0aceac1b7865ace03
ItzzExcel <excel.xcelx@icloud.com>
2023-05-27 20:00:04 -0600

    f60e28eb3327dc91b5b75439386b4dd1

---
+++ LInjector/Classes/CheckVersion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Octokit;
+
+namespace LInjector
+{
+    public class GitHubVersionChecker
+    {
+        private const string owner = "ItzzExcel";
+        private const string repo = "LInjector";
+
+        public static bool IsOutdatedVersion(string currentVersion)
+        {
+            if (currentVersion == "f81fb0e34f313b6cf0d0fc345890a33f")
+            {
+                return false;
+            }
+
+            var client = new GitHubClient(new ProductHeaderValue("CheckGitHubRelease"));
+
+            var releases = client.Repository.Release.GetAll(owner, repo).Result;
+
+            var latestRelease = releases
+                .Where(r => r.TagName.StartsWith("v"))
+                .OrderByDescending(r => r.PublishedAt)
+                .FirstOrDefault();
+
+            if (latestRelease != null)
+            {
+                var latestVersion = latestRelease.TagName.TrimStart('v');
+
+                var current = Version.Parse(currentVersion.TrimStart('v'));
+                var latest = Version.Parse(latestVersion);
+
+                return current < latest;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LInjector/Classes/infoManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LInjector
+{
+    public class NotificationManager
+    {
+        private bool isBusy;
+
+        public async Task FireNotification<T>(string message, T targetControl) where T : Control
+        {
+            if (isBusy)
+            {
+                while (isBusy)
+                {
+                    await Task.Delay(100);
+                }
+            }
+
+            isBusy = true;
+
+            targetControl.Text = "";
+
+            foreach (char character in message)
+            {
+                targetControl.Text += character;
+                await Task.Delay(30);
+            }
+
+            await Task.Delay(2500);
+
+            targetControl.Text = "";
+            foreach (char character in ". . .")
+            {
+                targetControl.Text += character;
+                await Task.Delay(30);
+            }
+
+            isBusy = false;
+        }
+    }
+}
