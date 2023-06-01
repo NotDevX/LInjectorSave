bf32ecd434a72f0364579df13f4de9700475642a
ItzzExcel <excel.xcelx@icloud.com>
2023-05-31 23:30:28 -0600

    14c5902b4c0ec4831fc7964004f18e97

--- LInjector/Program.cs
+++ LInjector/Program.cs
@@ -19,7 +19,7 @@ namespace LInjector
         /// </summary>
         /// 
 
-        public const string currentVersion = "v28.05.2023"; 
+        public const string currentVersion = "v31.05.2023"; 
         // Put "f81fb0e34f313b6cf0d0fc345890a33f" for skipping isOutdated MessageBox. 
         // The versions are in format dd/MM/yyy. (adding the v), if it's December 31, 1969, the version is "v31.12.1969"
 
