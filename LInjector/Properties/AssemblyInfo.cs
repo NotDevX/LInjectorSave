5180a49612df08b9061a01c14e2ad00838ed9c13
ItzzExcel <excel.xcel@icloud.com>
2023-06-06 23:30:59 -0600

    a5476c0c66153c544c42090205b1ecf3

--- LInjector/Properties/AssemblyInfo.cs
+++ LInjector/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5")]
+[assembly: AssemblyVersion("3.5")]
 // LInjector UI Version https://github.com/ItzzExcel/LInjector
 
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
