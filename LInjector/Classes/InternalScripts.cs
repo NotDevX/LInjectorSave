42e9ea64d8ef8864a1dc55e1e6f2ddf0c916cfaf
ItzzExcel <excel.xcel@icloud.com>
2023-07-21 23:53:31 -0600

    Internal Script Update! With a lot of help by @depthso https://github.com/depthso/LInjector
    Hash: 3274688ee8468b5e8ebd4c7c0fd937d4

---
+++ LInjector/Classes/InternalScripts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LInjector.Classes
+{
+    public static class InternalFunctions
+    {
+        private static readonly string ScriptPath = ".\\Resources\\scripts\\functions.lua";
+        private static readonly string ScriptContent = File.ReadAllText(ScriptPath);
+        private static application GetApplication = new application();
+        public static void Load()
+        {
+            if (!File.Exists(ScriptPath))
+            {
+                ThreadBox.MsgThread($"Couldn't find the path of {ScriptPath}, retry downloading LInjector.",
+                                    "LInjector | Warning",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var flag = FluxusAPI.is_injected(FluxusAPI.pid);
+                if (flag)
+                {
+                    FluxusAPI.run_script(FluxusAPI.pid, ScriptContent);
+                } else
+                {
+                    _ = NotificationManager.FireNotification("Not injected", GetApplication.infSettings);
+                }
+            }
+        }
+    }
+}
---
+++ LInjector/Resources/scripts/functions.lua
@@ -0,0 +1,64 @@
+﻿ --[[
+ *
+ * ░▒█░░░░▀█▀░█▀▀▄░░░▀░█▀▀░█▀▄░▀█▀░▄▀▀▄░█▀▀▄░░░▒█▀▀▀░█░▒█░█▀▀▄░█▀▄░▀█▀░░▀░░▄▀▀▄░█▀▀▄░█▀▀░░
+ * ░▒█░░░░▒█░░█░▒█░░░█░█▀▀░█░░░░█░░█░░█░█▄▄▀░░░▒█▀▀░░█░▒█░█░▒█░█░░░░█░░░█▀░█░░█░█░▒█░▀▀▄░░
+ * ░▒█▄▄█░▄█▄░▀░░▀░█▄█░▀▀▀░▀▀▀░░▀░░░▀▀░░▀░▀▀░░░▒█░░░░░▀▀▀░▀░░▀░▀▀▀░░▀░░▀▀▀░░▀▀░░▀░░▀░▀▀▀░░
+ *
+ * Created by depso (https://github.com/depthso)
+ *
+ *
+ * ]]
+
+LILibs = {}
+
+function Export(NameCaller, SubRoutine)
+    getgenv()[NameCaller] = SubRoutine
+end
+function DebugOut(String)
+    warn(("%s: %s"):format("LInjector",String))
+end
+
+Export("identifyexecutor", function()
+    return "LInjector UWP"
+end)
+
+Export("getexecutorname", function()
+	return "LInjector"
+end)
+
+Export("LInjector", true)
+
+function LILibs.Hello_World( ) -- LI_Hello_World
+    return print("Hello world!")
+end
+function LILibs.copy(String)
+    local SetClipBoard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
+	if SetClipBoard then
+        SetClipBoard(String)
+        return true
+    else
+        DebugOut("Unable to set clipboard")
+    end
+end
+function LILibs.getRoot(CharacterModel)
+	local rootPart = CharacterModel:FindFirstChild('HumanoidRootPart') or CharacterModel:FindFirstChild('Torso') or CharacterModel:FindFirstChild('UpperTorso')
+	return rootPart
+end
+function LILibs.Isr15(CharacterModel)
+	if CharacterModel:FindFirstChildOfClass('Humanoid').RigType == Enum.HumanoidRigType.R15 then
+		return true
+	end
+    return false
+end
+function LILibs.isNumber(str)
+	if tonumber(str) ~= nil or str == 'inf' then
+		return true
+	end
+    return false
+end
+
+for FunctionName in pairs(LILibs) do 
+    getgenv()["LI_"..FunctionName] = LILibs[FunctionName] 
+end
+
+loadstring(game:HttpGet("https://github.com/ItzzExcel/LInjectorRedistributables/raw/main/injectionscreen.lua", true))
